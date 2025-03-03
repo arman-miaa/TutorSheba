a2ed9cb2e2e2b4601259609347e253f8fff08107
Arman <mdarman365506@gmail.com>
2025-03-03 15:45:19 +0600

    WantToBeCome section design done

--- tutorsheba-client/src/components/WantToBeCome.jsx
+++ tutorsheba-client/src/components/WantToBeCome.jsx
@@ -1,18 +1,21 @@
-
+import {Link} from "react-router"
 
 const WantToBeCome = () => {
   return (
     <div className=" container mx-auto mt-20   ">
-      <div className="flex justify-center items-center gap-4 bg-base-300 p-10 mx-24">
+      <div className="flex justify-center items-center gap-4 bg-base-300 shadow-2xl p-10 mx-24 lg:mx-48">
         <div>
-          <h1>Want to become</h1>
-          <h2>Tutor</h2>
+          <h1 className="uppercase text-gray-500">Want to become</h1>
+          <h2 className="uppercase text-3xl lg:text-5xl font-extrabold">Tutor</h2>
         </div>
         <div>
-          <p>Let’s Work Together & Explore Opportunities</p>
+          <p>Let’s Work Together <br /> & Explore Opportunities</p>
         </div>
         <div>
-          <button className="btn btn-primary">Register</button>
+          <Link to="/register">
+            {" "}
+            <button className="btn bg-black text-white font-bold mt-8">Register</button>
+          </Link>
         </div>
       </div>
     </div>
